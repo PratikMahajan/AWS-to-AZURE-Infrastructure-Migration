40c49feadd37a35f59640d5cea7ce15427e26eda
AkashJ-487 <jagtap.ak@northeastern.edu>
2019-10-09 05:20:32 -0400

    Adding vpc resources

---
+++ vpc.tf
@@ -0,0 +1,147 @@
+
+# Define our VPC
+resource "aws_vpc" "default" {
+  # name = "${var.vpc_name}"
+  cidr_block = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+
+ tags = {
+    Name = "${var.vpc_name}"
+  }
+}
+
+# Define the public subnet
+resource "aws_subnet" "public-subnet" {
+  # name = "${var.subnet1_name}"
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.subnet1_cidr}"
+  availability_zone = "${var.subnet1_az}"
+
+ tags = {
+    Name = "${var.subnet1_name}"
+  }
+}
+
+# Define the private subnet
+resource "aws_subnet" "private-subnet1" {
+  # name = "${var.subnet2_name}"
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.subnet2_cidr}"
+  availability_zone = "${var.subnet2_az}"
+
+ tags = {
+    Name = "${var.subnet2_name}"
+  }
+}
+resource "aws_subnet" "private-subnet2" {
+  # name = "${var.subnet2_name}"
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "${var.subnet3_cidr}"
+  availability_zone = "${var.subnet3_az}"
+
+ tags = {
+    Name = "${var.subnet3_name}"
+  }
+}
+
+# Define the internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.default.id}"
+
+ tags = {
+    Name = "VPC IGW"
+  }
+}
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+ tags = {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
+
+# Define the security group for public subnet
+resource "aws_security_group" "sgpublic" {
+  name = "vpc_test_public"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.default.id}"
+
+ tags = {
+    Name = "Public SG"
+  }
+}
+
+# Define the security group for private subnet
+resource "aws_security_group" "sgprivate"{
+  name = "sg_test_public"
+  description = "Allow traffic from public subnet"
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["${var.subnet1_cidr}"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["${var.subnet1_cidr}"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.subnet1_cidr}"]
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+ tags = {
+    Name = "Private SG"
+  }
+}

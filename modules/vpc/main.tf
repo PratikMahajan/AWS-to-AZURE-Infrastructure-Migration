786721b028181c30b046276532974c17bf80ac71
pratikmahajan <pratik@mahajan.xyz>
2019-10-18 20:36:50 -0400

    more readable vpc.tf code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,7 @@
 
 # Define our VPC
 resource "aws_vpc" "default" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
 
  tags = {
@@ -11,19 +11,19 @@ resource "aws_vpc" "default" {
 
 # Define the public subnet
 resource "aws_subnet" "public-subnet" {
-  vpc_id = aws_vpc.default.id
-  cidr_block = var.subnet1_cidr
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.subnet1_cidr
   availability_zone = var.subnet1_az
 
  tags = {
-    Name = var.subnet1_name
+    Name = "${var.env}-var.subnet1_name"
   }
 }
 
 # Define the private subnet
 resource "aws_subnet" "private-subnet1" {
-  vpc_id = aws_vpc.default.id
-  cidr_block = var.subnet2_cidr
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.subnet2_cidr
   availability_zone = var.subnet2_az
 
  tags = {
@@ -32,8 +32,8 @@ resource "aws_subnet" "private-subnet1" {
 }
 
 resource "aws_subnet" "private-subnet2" {
-  vpc_id = aws_vpc.default.id
-  cidr_block = var.subnet3_cidr
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.subnet3_cidr
   availability_zone = var.subnet3_az
 
  tags = {
@@ -66,52 +66,52 @@ resource "aws_route_table" "web-public-rt" {
 
 # Assign the route table to the public Subnet
 resource "aws_route_table_association" "subnet1" {
-  subnet_id = aws_subnet.public-subnet.id
-  route_table_id = aws_route_table.web-public-rt.id
+  subnet_id       = aws_subnet.public-subnet.id
+  route_table_id  = aws_route_table.web-public-rt.id
 }
 resource "aws_route_table_association" "subnet2" {
-  subnet_id = aws_subnet.private-subnet1.id
-  route_table_id = aws_route_table.web-public-rt.id
+  subnet_id       = aws_subnet.private-subnet1.id
+  route_table_id  = aws_route_table.web-public-rt.id
 }
 resource "aws_route_table_association" "subnet3" {
-  subnet_id = aws_subnet.private-subnet2.id
-  route_table_id = aws_route_table.web-public-rt.id
+  subnet_id       = aws_subnet.private-subnet2.id
+  route_table_id  = aws_route_table.web-public-rt.id
 }
 
 # Define the security group for public subnet
 resource "aws_security_group" "sgpublic" {
-  name = "vpc_test_public"
+  name        = "vpc_test_public"
   description = "Allow incoming HTTP connections & SSH access"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port     = 80
+    to_port       = 80
+    protocol      = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port     = 443
+    to_port       = 443
+    protocol      = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = -1
-    to_port = -1
-    protocol = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port     = -1
+    to_port       = -1
+    protocol      = "icmp"
+    cidr_blocks   = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks =  ["0.0.0.0/0"]
+    from_port     = 22
+    to_port       = 22
+    protocol      = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
   }
 
-  vpc_id=aws_vpc.default.id
+  vpc_id = aws_vpc.default.id
 
  tags = {
     Name = "Public SG"
@@ -120,27 +120,27 @@ resource "aws_security_group" "sgpublic" {
 
 # Define the security group for private subnet
 resource "aws_security_group" "sgprivate"{
-  name = "sg_test_public"
+  name        = "sg_test_public"
   description = "Allow traffic from public subnet"
 
   ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
     cidr_blocks = [var.subnet1_cidr]
   }
 
   ingress {
-    from_port = -1
-    to_port = -1
-    protocol = "icmp"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
     cidr_blocks = [var.subnet1_cidr]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = [var.subnet1_cidr]
   }
 

d50ba0c3e5e7500d6f462432748a64fbae784827
pratikmahajan <pratik@mahajan.xyz>
2019-10-20 18:21:16 -0400

    added variabled for ec2 instance terraform

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -6,6 +6,7 @@ data "aws_ami" "centos" {
 resource "aws_instance" "webec2" {
   ami             = "${data.aws_ami.centos.id}"
   instance_type   = "t2.micro"
+  disable_api_termination = var.ec2_termination_disable
 
   vpc_security_group_ids = var.aws_ec2_security_group
 
---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_account_id" {}
+variable "aws_ec2_security_group" {}
+variable "ebs_block_name" {}
+variable "ebs_volume_type" {}
+variable "ebs_volume_size" {}
+variable "ebs_delete_on_termination" {}
+variable "env" {}
+variable "ec2_termination_disable" {}

84061ad5887de5bf9c5a0af55a980d0c63928f9c
pratikmahajan <pratik@mahajan.xyz>
2019-11-17 00:03:46 -0500

    added certificates to gitignore

--- modules/ec2_load_balancer/main.tf
+++ modules/ec2_load_balancer/main.tf
@@ -11,3 +11,24 @@ resource "aws_lb" "loadbalancer" {
     env = var.env
   }
 }
+
+
+resource "aws_lb_target_group" "lb_target_group" {
+  name     = "ec2_recipe_lb_target_group"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = var.aws_vpc_id
+}
+
+resource "aws_lb_listener" "lb_listner" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group.arn
+  }
+}

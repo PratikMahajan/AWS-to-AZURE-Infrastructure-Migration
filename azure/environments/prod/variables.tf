3a8899d0a0892bcf51269c5cb516adf4498ea0d0
AkashJ-487 <jagtap.ak@northeastern.edu>
2019-12-02 01:23:39 -0500

    added mariadb variable from pratiks branch

--- azure/environments/prod/variables.tf
+++ azure/environments/prod/variables.tf
@@ -7,6 +7,13 @@ variable "subnet3_addr" {}
 
 variable "storage_account_name" {}
 
+variable "maria_db_server_scale_capacity" {}
+variable "maria_db_storage_mb" {}
+variable "mariadb_admin_login_password" {}
+variable "mariadb_admin_login_username" {}
+variable "mariadb_name" {}
+variable "mariadb_ssl_enforcement" {}
+
 variable "topic" {}
 
 variable "function_bolb_name" {}

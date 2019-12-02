0b38f97d488fe0d9da6f445c78a1fac2378d8595
AkashJ-487 <jagtap.ak@northeastern.edu>
2019-12-01 23:46:04 -0500

    added connection string as output variable

--- azure/modules/storage_account/output.tf
+++ azure/modules/storage_account/output.tf
@@ -5,3 +5,7 @@ output "storage_account_name" {
 output "storage_account_id" {
   value = azurerm_storage_account.storage_account.id
 }
+
+output "storage_account_connection_string" {
+  value = azurerm_storage_account.storage_account.primary_connection_string
+}

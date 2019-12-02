47a4f38a43c80a39704c4e6ff2daf396d840d7d9
AkashJ-487 <jagtap.ak@northeastern.edu>
2019-12-01 22:02:11 -0500

    declared variabes for rds

--- azure/environments/prod/variables.tf
+++ azure/environments/prod/variables.tf
@@ -6,3 +6,9 @@ variable "subnet2_addr" {}
 variable "subnet3_addr" {}
 
 variable "storage_account_name" {}
+
+variable "sql_server_name" {}
+variable "sql_server_version" {}
+variable "admin_login" {}
+variable "admin_pass" {}
+variable "sql_db_name" {}

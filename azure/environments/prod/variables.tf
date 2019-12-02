eb0f7d2b659cb7a23a8ebaebf2afedc6a204ddbe
AkashJ-487 <jagtap.ak@northeastern.edu>
2019-12-02 03:09:44 -0500

    added variables required for cosmos db

--- azure/environments/prod/variables.tf
+++ azure/environments/prod/variables.tf
@@ -16,4 +16,8 @@ variable "mariadb_ssl_enforcement" {}
 
 variable "topic" {}
 
+variable "cosmos_acct_name" {}
+variable "cosmos_tbl_name" {}
+variable "failover_loc" {}
+
 variable "function_bolb_name" {}

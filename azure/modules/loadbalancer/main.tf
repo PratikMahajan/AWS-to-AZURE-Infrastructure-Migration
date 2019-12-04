0b7852c9cfdb8ee1d34d13fc7e6b9fa609e96da3
pratikmahajan <pratik@mahajan.xyz>
2019-12-04 18:27:31 -0500

    added waf prevention rule

--- azure/modules/loadbalancer/main.tf
+++ azure/modules/loadbalancer/main.tf
@@ -68,6 +68,12 @@ resource "azurerm_application_gateway" "network" {
     backend_address_pool_name  = "${local.backend_address_pool_name}"
     backend_http_settings_name = "${local.http_setting_name}"
   }
+
+  waf_configuration {
+    enabled = true
+    firewall_mode = "Prevention"
+    rule_set_version = "3.1"
+  }
 }
 
 
